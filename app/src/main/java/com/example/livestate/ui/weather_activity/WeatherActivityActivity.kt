7d6f5719757f1879bd1361942e0d92b67ac1da79
tenlatu01042002@gmail.com <tenlatu01042002@gmail.com>
2025-06-26 11:24:34 +0700

    fist comit font

---
+++ app/src/main/java/com/example/livestate/ui/weather_activity/WeatherActivityActivity.kt
@@ -0,0 +1,29 @@
+package com.example.livestate.ui.weather_activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.livestate.R
+import com.example.livestate.base.BaseActivity
+import com.example.livestate.databinding.ActivityWeatherActivityBinding
+
+class WeatherActivityActivity : BaseActivity<ActivityWeatherActivityBinding>() {
+    override fun setViewBinding(): ActivityWeatherActivityBinding {
+        return ActivityWeatherActivityBinding.inflate(layoutInflater)
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun viewListener() {
+        TODO("Not yet implemented")
+    }
+
+    override fun dataObservable() {
+        TODO("Not yet implemented")
+    }
+
+}
