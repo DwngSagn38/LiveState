bb7ec082e5d9897dc1b3116e3f8445b5245df73f
tenlatu01042002@gmail.com <tenlatu01042002@gmail.com>
2025-07-02 15:04:04 +0700

    WeatherActivity fix bug

--- app/src/main/java/com/example/livestate/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/livestate/ui/main/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.example.livestate.ui.main
 
-import android.content.pm.PackageManager
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.livestate.R
 import com.example.livestate.base.BaseActivity
 import com.example.livestate.databinding.ActivityMainBinding
 import com.example.livestate.ui.Speedometer.SpeedometerActivity
+import com.example.livestate.ui.cameracompass.CameraCompassActivity
+import com.example.livestate.ui.currency.CurrencyActivity
 import com.example.livestate.ui.earth3d.TheEarthActivity
 import com.example.livestate.ui.my_location.MyLocationActivity
 import com.example.livestate.ui.nearby_places.NearbyPlacesActivity
-import com.example.livestate.ui.nearby_places.NearbyPlacesDetailActivity
 import com.example.livestate.ui.route_map.RouteMapActivity
-import com.example.livestate.ui.cameracompass.CameraCompassActivity
-import com.example.livestate.ui.currency.CurrencyActivity
 import com.example.livestate.ui.setting.SettingActivity
-import com.example.livestate.widget.tap
 import com.example.livestate.ui.weather_activity.WeatherActivity
 import com.example.livestate.ui.world_clock.WorldClockActivity
 import com.example.livestate.widget.tap
@@ -138,37 +136,33 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (requestCode == 2001 && grantResults.isNotEmpty()
-            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            showActivity(WeatherActivity::class.java)
-        } else {
-            Toast.makeText(this, getString(R.string.grant_location_permission_to_view_details), Toast.LENGTH_SHORT).show()
-        if (requestCode == 1001 && grantResults.isNotEmpty() &&
-            grantResults[0] == PackageManager.PERMISSION_GRANTED
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
         ) {
-            pendingActivityClass?.let {
-                showActivity(it)
-            }
-            pendingActivityClass = null
+            showActivity(WeatherActivity::class.java)
         } else {
             Toast.makeText(
                 this,
                 getString(R.string.grant_location_permission_to_view_details),
                 Toast.LENGTH_SHORT
             ).show()
+            if (requestCode == 1001 && grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                pendingActivityClass?.let {
+                    showActivity(it)
+                }
+                pendingActivityClass = null
+            } else {
+                Toast.makeText(
+                    this,
+                    getString(R.string.grant_location_permission_to_view_details),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
 
-    private fun hasAllPermissions(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.CAMERA
-        ) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
-    }
 
     override fun dataObservable() {}
 }
